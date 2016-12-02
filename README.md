e3b0921481a270c6f914ba9d76a193fa3671b65f
Ryan Finnie <ryan@finnie.org>
2016-12-02 09:19:20 -0800

    Add Z-Wave Repeater

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 *Not guaranteed to be smart.*
 
 ## Device Handlers
+
+### Z-Wave Repeater
+
+Does (almost) literally nothing.
+
+While there are a few standalone commercial Z-Wave repeaters out there, this handler was built with the Iris Smart Plug in mind.
+The Iris Smart Plug is a ZigBee switched outlet, but also has the interesting feature that it acts as a Z-Wave repeater, but otherwise has no other Z-Wave function.
+However, when the Z-Wave side is added to SmartThings, it also shows up as a switched outlet, but does nothing when interacted with.
+A side effect of this is if you have Alexa intergration, they will be exported to Alexa as usable Things, which are not desired.
+
+To install, add to the Developer API, then edit your existing Z-Wave Thing and change Type to "Z-Wave Repeater".
+It will appear in the app with the primary function being Refresh, which effectively does nothing except query the device firmware version.
+These repeaters should also not show up in Alexa.
---
+++ devicetypes/rfinnie/zwave-repeater.src/zwave-repeater.groovy
@@ -0,0 +1,65 @@
+/**
+ *  Copyright 2016 Ryan Finnie
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Z-Wave Repeater", namespace: "rfinnie", author: "Ryan Finnie") {
+		capability "Refresh"
+	}
+
+	tiles(scale: 2) {
+		standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 6, height: 4) {
+			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
+		main "refresh"
+		details(["refresh"])
+	}
+}
+
+def parse(String description) {
+	log.debug "description is $description"
+	def result = []
+	if (description.startsWith("Err")) {
+		result = createEvent(descriptionText:description, isStateChange:true)
+	} else {
+		def cmd = zwave.parse(description, [0x20: 1, 0x84: 1, 0x98: 1, 0x56: 1, 0x60: 3])
+		if (cmd) {
+			result += zwaveEvent(cmd)
+		}
+	}
+	return result
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.versionv1.VersionReport cmd) {
+	def fw = "${cmd.applicationVersion}.${cmd.applicationSubVersion}"
+	def protocol = "${cmd.zWaveProtocolVersion}.${cmd.zWaveProtocolSubVersion}"
+	def library = "${cmd.zWaveLibraryType}"
+	log.debug "Firmware: $fw - Protocol: $protocol - Library: $library"
+	updateDataValue("fw", fw)
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd) {
+	def msr = String.format("%04X-%04X-%04X", cmd.manufacturerId, cmd.productTypeId, cmd.productId)
+	log.debug "MSR: $msr"
+	updateDataValue("MSR", msr)
+}
+
+def ping() {
+	refresh()
+}
+
+def refresh() {
+	delayBetween([
+		zwave.versionV1.versionGet().format(),
+		zwave.manufacturerSpecificV2.manufacturerSpecificGet().format()
+	], 500)
+}
