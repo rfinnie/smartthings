3daeee1c953dacd9aae0f17ee5f81e850ff0d22d
Ryan Finnie <ryan@finnie.org>
2020-09-18 22:23:00 -0700

    Black hs100_proxy.py

--- devicetypes/rfinnie/tplink-hs100-lan-web-proxy.src/hs100_proxy.py
+++ devicetypes/rfinnie/tplink-hs100-lan-web-proxy.src/hs100_proxy.py
@@ -29,16 +29,16 @@ class HS100Handler(http.server.BaseHTTPRequestHandler):
     config = None
 
     def decode(self, a):
-        code = 0xab
-        b = b''
+        code = 0xAB
+        b = b""
         for i in a:
             b += bytes([i ^ code])
             code = i
         return b
 
     def encode(self, a):
-        code = 0xab
-        b = b''
+        code = 0xAB
+        b = b""
         for i in a:
             code = i ^ code
             b += bytes([code])
@@ -48,131 +48,114 @@ class HS100Handler(http.server.BaseHTTPRequestHandler):
         sock = socket.socket()
         sock.connect((host, port))
 
-        sock.send(struct.pack('!l', len(command)) + self.encode(command))
+        sock.send(struct.pack("!l", len(command)) + self.encode(command))
 
         result = self.decode(sock.recv(8192)[4:])
         sock.close()
 
         return result
 
-    def log_request(self, code='-', size='-', user_agent='-', extra='-'):
-        if self.headers.get('User-Agent'):
-            user_agent = self.headers.get('User-Agent')
+    def log_request(self, code="-", size="-", user_agent="-", extra="-"):
+        if self.headers.get("User-Agent"):
+            user_agent = self.headers.get("User-Agent")
         if self.post_body:
-            extra = self.post_body.decode('UTF-8')
-        extra = extra.replace('\r', '').replace('\n', '')
+            extra = self.post_body.decode("UTF-8")
+        extra = extra.replace("\r", "").replace("\n", "")
         if len(extra) > 100:
-            extra = extra[0:97] + '...'
-        self.log_message('"{}" {} {} "-" "{}" "{}"'.format(
-            self.requestline,
-            str(code),
-            str(size),
-            user_agent,
-            extra,
-        ))
+            extra = extra[0:97] + "..."
+        self.log_message(
+            '"{}" {} {} "-" "{}" "{}"'.format(
+                self.requestline, str(code), str(size), user_agent, extra
+            )
+        )
 
     def error_500(self, message):
-        message_bytes = message.encode('UTF-8')
+        message_bytes = message.encode("UTF-8")
         self.send_response(500)
-        self.send_header('Content-Type', 'text/plain; charset=utf-8')
-        self.send_header('Content-Length', len(message_bytes))
+        self.send_header("Content-Type", "text/plain; charset=utf-8")
+        self.send_header("Content-Length", len(message_bytes))
         self.end_headers()
         self.wfile.write(message_bytes)
 
     def do_POST(self):
-        if not self.headers.get('Content-Length'):
+        if not self.headers.get("Content-Length"):
             self.send_response(400)
-            self.send_header('Content-Type', 'text/plain; charset=utf-8')
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
             self.end_headers()
-            self.wfile.write('Content-Length required.'.encode('UTF-8'))
+            self.wfile.write("Content-Length required.".encode("UTF-8"))
             return
-        command_data = self.rfile.read(
-            int(self.headers.get('Content-Length'))
-        )
+        command_data = self.rfile.read(int(self.headers.get("Content-Length")))
         self.post_body = command_data
         try:
             result = self.send_command(
-                self.config.hs100_addr,
-                self.config.hs100_port,
-                command_data
+                self.config.hs100_addr, self.config.hs100_port, command_data
             )
         except socket.error as e:
             return self.error_500(str(e))
         except Exception as e:
-            return self.error_500('Unknown exception: ' + str(e))
+            return self.error_500("Unknown exception: " + str(e))
         self.send_response(200)
-        self.send_header('Content-Type', 'application/json; charset=utf-8')
-        self.send_header('Content-Length', len(result))
+        self.send_header("Content-Type", "application/json; charset=utf-8")
+        self.send_header("Content-Length", len(result))
         self.end_headers()
         self.wfile.write(result)
 
     def result_status_text(self, r):
-        output = 'SERVER INFORMATION\n'
-        output += '    {:16}: {}\n'.format(
-            'Date', self.date_time_string()
+        output = "SERVER INFORMATION\n"
+        output += "    {:16}: {}\n".format("Date", self.date_time_string())
+        output += "    {:16}: {}:{}\n".format(
+            "Device", self.config.hs100_addr, self.config.hs100_port
         )
-        output += '    {:16}: {}:{}\n'.format(
-            'Device', self.config.hs100_addr, self.config.hs100_port
+        output += "    {:16}: {}:{}\n".format(
+            "Local", self.config.local_addr, self.config.local_port
         )
-        output += '    {:16}: {}:{}\n'.format(
-            'Local', self.config.local_addr, self.config.local_port
+        output += "    {:16}: {}:{}\n".format(
+            "Remote", self.client_address[0], self.client_address[1]
         )
-        output += '    {:16}: {}:{}\n'.format(
-            'Remote', self.client_address[0], self.client_address[1]
-        )
-        output += '    {:16}: {}\n'.format(
-            'User agent', self.headers.get('User-Agent')
-        )
+        output += "    {:16}: {}\n".format("User agent", self.headers.get("User-Agent"))
 
-        output += '\nDEVICE STATUS\n'
-        for (k, v) in sorted(r['system']['get_sysinfo'].items()):
-            output += '    {:16}: {}\n'.format(k, v)
+        output += "\nDEVICE STATUS\n"
+        for (k, v) in sorted(r["system"]["get_sysinfo"].items()):
+            output += "    {:16}: {}\n".format(k, v)
 
-        output += '\nEXAMPLES\n\n'
+        output += "\nEXAMPLES\n\n"
 
         def example_curl(command):
-            return '    {}\n\n'.format(
-                'curl -s --data-binary \'{}\' -H "Content-type: application/json" http://{}/command | json_pp'.format(
-                    json.dumps(command),
-                    self.headers.get('Host'),
+            return "    {}\n\n".format(
+                "curl -s --data-binary '{}' -H \"Content-type: application/json\" http://{}/command | json_pp".format(
+                    json.dumps(command), self.headers.get("Host")
                 )
             )
 
-        output += 'Device status:\n'
-        output += example_curl({'system':{'get_sysinfo':{'state': None}}})
+        output += "Device status:\n"
+        output += example_curl({"system": {"get_sysinfo": {"state": None}}})
 
-        output += 'Turn on outlet:\n'
-        output += example_curl({'system':{'set_relay_state':{'state': 1}}})
+        output += "Turn on outlet:\n"
+        output += example_curl({"system": {"set_relay_state": {"state": 1}}})
 
-        output += 'Turn off outlet:\n'
-        output += example_curl({'system':{'set_relay_state':{'state': 0}}})
+        output += "Turn off outlet:\n"
+        output += example_curl({"system": {"set_relay_state": {"state": 0}}})
 
         return output
 
     def do_GET(self):
-        command_data = json.dumps({
-            'system': {
-                'get_sysinfo': {
-                    'state': None,
-                }
-            }
-        }).encode('UTF-8')
+        command_data = json.dumps({"system": {"get_sysinfo": {"state": None}}}).encode(
+            "UTF-8"
+        )
         try:
             result = self.send_command(
-                self.config.hs100_addr,
-                self.config.hs100_port,
-                command_data
+                self.config.hs100_addr, self.config.hs100_port, command_data
             )
         except socket.error as e:
             return self.error_500(str(e))
         except Exception as e:
-            return self.error_500('Unknown exception: ' + str(e))
-        result_d = json.loads(result.decode('UTF-8'))
+            return self.error_500("Unknown exception: " + str(e))
+        result_d = json.loads(result.decode("UTF-8"))
         output = self.result_status_text(result_d)
-        output_bytes = output.encode('UTF-8')
+        output_bytes = output.encode("UTF-8")
         self.send_response(200)
-        self.send_header('Content-Type', 'text/plain; charset=utf-8')
-        self.send_header('Content-Length', len(output_bytes))
+        self.send_header("Content-Type", "text/plain; charset=utf-8")
+        self.send_header("Content-Length", len(output_bytes))
         self.end_headers()
         self.wfile.write(output_bytes)
 
@@ -181,29 +164,25 @@ def parse_args():
     import argparse
 
     parser = argparse.ArgumentParser(
-        description='hs100_proxy - TP-LINK Smart Plug HS100 LAN web proxy',
+        description="hs100_proxy - TP-LINK Smart Plug HS100 LAN web proxy",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        '--local-port', '-l', type=int, default=8362,
-        help='local port for web server',
+        "--local-port", "-l", type=int, default=8362, help="local port for web server"
     )
     parser.add_argument(
-        '--local-addr', type=str, default='0.0.0.0',
-        help='local address for web server',
+        "--local-addr", type=str, default="0.0.0.0", help="local address for web server"
     )
     parser.add_argument(
-        '--hs100-port', '-p', type=int, default=9999,
-        help='port for HS100 device',
+        "--hs100-port", "-p", type=int, default=9999, help="port for HS100 device"
     )
     parser.add_argument(
-        'hs100_addr', type=str,
-        help='address/hostname for HS100 device',
+        "hs100_addr", type=str, help="address/hostname for HS100 device"
     )
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     config = parse_args()
     server_class = http.server.HTTPServer
     httpd = server_class((config.local_addr, config.local_port), HS100Handler)
